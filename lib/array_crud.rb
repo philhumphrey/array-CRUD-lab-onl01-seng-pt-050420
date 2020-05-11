5e57d873ac3c34ea115239f1862c2de4ed747fc6
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-11 06:26:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
  array = ["wow", "I", "am", "really", "learning"]
     array << "happy"
+    
 end
 
 def add_element_to_start_of_array(array, element)
